c756ca88c2bbf0615bd6904b0ef8530d23e35461
malong5 <malong5@xiaomi.com>
2023-07-18 10:56:52 +0800

    req object

--- provider/src/main/java/com/melo/gateway/interfaces/HelloServiceImpl.java
+++ provider/src/main/java/com/melo/gateway/interfaces/HelloServiceImpl.java
@@ -1,5 +1,7 @@
 package com.melo.gateway.interfaces;
 
+import com.alibaba.fastjson.JSON;
+import com.melo.dto.XReq;
 import com.melo.rpc.HelloService;
 import org.apache.dubbo.config.annotation.Service;
 
@@ -9,5 +11,15 @@ public class HelloServiceImpl implements HelloService {
     @Override
     public String hello(String str) {
         return str + " melo " + System.currentTimeMillis();
+    }
+
+    @Override
+    public String add(XReq req) {
+        return "you add me " + JSON.toJSONString(req) + System.currentTimeMillis();
+    }
+
+    @Override
+    public String addPlus(String str, XReq req) {
+        return "you also add me str " + str + " req " + JSON.toJSONString(req) + System.currentTimeMillis();
     }
 }
--- rpc/src/main/java/com/melo/rpc/HelloService.java
+++ rpc/src/main/java/com/melo/rpc/HelloService.java
@@ -1,6 +1,12 @@
 package com.melo.rpc;
 
+import com.melo.dto.XReq;
+
 public interface HelloService {
 
     String hello(String str);
+
+    String add(XReq req);
+
+    String addPlus(String str, XReq req);
 }
